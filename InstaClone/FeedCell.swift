d3a116b0c1fabb17a7c82404b3187c10638e9707
Eyupkosee <89103093+Eyupkosee@users.noreply.github.com>
2023-05-13 13:22:05 +0300

    first commit

---
+++ InstaClone/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  InstaClone
+//
+//  Created by eyüp köse on 12.05.2023.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
